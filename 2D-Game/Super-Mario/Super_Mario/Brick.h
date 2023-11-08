f5475946b81245d0a4a3e92251cae73dbec8db6d
InfinitGamer <jeremycomino@gmail.com>
2023-11-08 10:11:57 +0100

    bricks done

--- 2D-Game/Super-Mario/Super_Mario/Brick.h
+++ 2D-Game/Super-Mario/Super_Mario/Brick.h
@@ -9,11 +9,16 @@ class Brick :
         enum BrickState {
             IDLE,
             ANIMATION,
+            ANIMATION_DESTROYED,
             DESTROYED
         };
         BrickState currentState;
         float currentTime;
-
+        Sprite* topLeft;
+        Sprite* topRight;
+        Sprite* bottomLeft;
+        Sprite* bottomRight;
+        glm::vec2 posTL, posTR, posBL, posBR;
     public:
         Brick(glm::vec2 tilePosition, glm::vec2 tileSize, glm::vec2 texturePosition, glm::vec2 textureSize, ShaderProgram& shader, Texture* text);
         Brick();
