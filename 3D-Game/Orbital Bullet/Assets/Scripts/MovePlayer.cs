1fc158672fb4b2ed15718ebaf399b11f22b3e461
InfinitGamer <jeremycomino@gmail.com>
2023-11-20 16:50:47 +0100

    cilindro puesto

--- 3D-Game/Orbital Bullet/Assets/Scripts/MovePlayer.cs
+++ 3D-Game/Orbital Bullet/Assets/Scripts/MovePlayer.cs
@@ -4,52 +4,89 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    public float moveSpeed = 3.0f, jumpSpeed = 5.0f;
-    public float gravity = 10.0f;
+    public float rotationSpeed, jumpSpeed, gravity;
 
-    public AudioClip jumpSound;
-
-    int orientation = 1;
-
-    Vector2 speed;
+    Vector3 startDirection;
+    float speedY;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Store starting direction of the player with respect to the axis of the level
+        startDirection = transform.position - transform.parent.position;
+        startDirection.y = 0.0f;
+        startDirection.Normalize();
+
+        speedY = 0;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        CharacterController charControl = GetComponent<CharacterController>();
+        Vector3 position;
+
+        // Left-right movement
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
-            if (orientation == 1)
-                transform.Rotate(0.0f, 180.0f, 0.0f);
-            speed.x = -moveSpeed;
-            orientation = 0;
+            float angle;
+            Vector3 direction, target;
+
+            position = transform.position;
+            angle = rotationSpeed * Time.deltaTime;
+            direction = position - transform.parent.position;
+            if (Input.GetKey(KeyCode.A))
+            {
+                target = transform.parent.position + Quaternion.AngleAxis(angle, Vector3.up) * direction;
+                if (charControl.Move(target - position) != CollisionFlags.None)
+                {
+                    transform.position = position;
+                    Physics.SyncTransforms();
+                }
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                target = transform.parent.position + Quaternion.AngleAxis(-angle, Vector3.up) * direction;
+                if (charControl.Move(target - position) != CollisionFlags.None)
+                {
+                    transform.position = position;
+                    Physics.SyncTransforms();
+                }
+            }
         }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+
+        // Correct orientation of player
+        // Compute current direction
+        Vector3 currentDirection = transform.position - transform.parent.position;
+        currentDirection.y = 0.0f;
+        currentDirection.Normalize();
+        // Change orientation of player accordingly
+        Quaternion orientation;
+        if ((startDirection - currentDirection).magnitude < 1e-3)
+            orientation = Quaternion.AngleAxis(0.0f, Vector3.up);
+        else if ((startDirection + currentDirection).magnitude < 1e-3)
+            orientation = Quaternion.AngleAxis(180.0f, Vector3.up);
+        else
+            orientation = Quaternion.FromToRotation(startDirection, currentDirection);
+        transform.rotation = orientation;
+
+        // Apply up-down movement
+        position = transform.position;
+        if (charControl.Move(speedY * Time.deltaTime * Vector3.up) != CollisionFlags.None)
         {
-            if (orientation == 0)
-                transform.Rotate(0.0f, 180.0f, 0.0f);
-            speed.x = moveSpeed;
-            orientation = 1;
+            transform.position = position;
+            Physics.SyncTransforms();
+        }
+        if (charControl.isGrounded)
+        {
+            if (speedY < 0.0f)
+                speedY = 0.0f;
+            if (Input.GetKey(KeyCode.W))
+                speedY = jumpSpeed;
         }
         else
-            speed.x = 0.0f;
-
-        if ((speed.y < 0) && GetComponent<CharacterController>().isGrounded)
-            speed.y = 0.0f;
-        if (Input.GetKey(KeyCode.W) && GetComponent<CharacterController>().isGrounded)
-        {
-            AudioSource.PlayClipAtPoint(jumpSound, transform.position);
-            speed.y = jumpSpeed;
-        }
-
-        speed.y -= gravity * Time.deltaTime;
-
-        Vector2 displace = speed * Time.deltaTime;
-        GetComponent<CharacterController>().Move(new Vector3(displace.x, displace.y, 0.0f));
+            speedY -= gravity * Time.deltaTime;
     }
 }
+
+
