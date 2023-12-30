c87c2921fb1f3d22bea97f61a53841ae2f754f3e
InfinitGamer <jeremycomino@gmail.com>
2023-12-30 19:39:18 +0100

    gui done

--- 3D-Game/Orbital Bullet/Assets/Scripts/WeaponBase.cs
+++ 3D-Game/Orbital Bullet/Assets/Scripts/WeaponBase.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 public abstract class WeaponBase : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    protected int ammo;
+    CurrentAmmo text;
     public abstract void Shoot(Vector3 pos, Quaternion initialRotation, Transform parent, float orientation);
+    protected void Start() {
+        text = GameObject.Find("CurrentAmmo").GetComponent<CurrentAmmo>();
+    }
+    void Update() {
+        if (gameObject.activeSelf)
+        {
+            text.SetAmmo(ammo);
+        }
+        
+    }
 }
--- 3D-Game/Orbital Bullet/Assets/Scripts/WeaponLong.cs
+++ 3D-Game/Orbital Bullet/Assets/Scripts/WeaponLong.cs
@@ -6,13 +6,22 @@ public class WeaponLong : WeaponBase
 {
     // Start is called before the first frame update
     public GameObject prefab;
+    const int maxAmmo = 50;
+    void Start() { 
+        base.Start();
+        ammo = maxAmmo;
+    }
     public override void Shoot(Vector3 pos, Quaternion initialRotation, Transform parent, float orientation)
     {
-        GameObject newObject = Instantiate(prefab, pos, initialRotation * prefab.transform.rotation, parent);
+        if (ammo > 0) {
+            GameObject newObject = Instantiate(prefab, pos, initialRotation * prefab.transform.rotation, parent);
 
-        BalaLongPlayer bEnemy = newObject.AddComponent<BalaLongPlayer>();
-        Debug.Log("orientation: " + orientation);
-        bEnemy.init();
-        bEnemy.setOrientation(orientation);
+            BalaLongPlayer bEnemy = newObject.AddComponent<BalaLongPlayer>();
+            Debug.Log("orientation: " + orientation);
+            bEnemy.init();
+            bEnemy.setOrientation(orientation);
+            --ammo;
+        }
+        
     }
 }
